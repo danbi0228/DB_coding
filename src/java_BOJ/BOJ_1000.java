98de48bf43a3660362f3a80e0b92c42f47dae98e
danbi0228 <qoeksql97@gmail.com>
2022-11-14 17:44:12 +0900

    2. A+B 두 수를 입력받고 합을 출력하는 문제

--- src/java_BOJ/BOJ_1000.java
+++ src/java_BOJ/BOJ_1000.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class BOJ_1000 {
     public static void main(String[] args) {
-//		Q. 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
