f376610db582cf9e9daafb84e350c440b0db399f
danbi0228 <qoeksql97@gmail.com>
2022-11-15 20:47:29 +0900

    5. A/B 나눗셈 문제

---
+++ src/java_BOJ/BOJ_1008.java
@@ -0,0 +1,14 @@
+package java_BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_1008 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.println(a / b);
+    }
+}
