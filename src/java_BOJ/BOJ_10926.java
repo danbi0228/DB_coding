c371bd2a756e4973e3f4f262677945a436460147
danbi0228 <qoeksql97@gmail.com>
2022-11-15 20:49:54 +0900

    7. ??! 입출력을 응용하는 문제

---
+++ src/java_BOJ/BOJ_10926.java
@@ -0,0 +1,13 @@
+package java_BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_10926 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+
+        System.out.println(a + "??!");
+    }
+}
