b9e999605a78d7dfebe564651c4920ebd516c049
danbi0228 <qoeksql97@gmail.com>
2022-11-16 23:07:07 +0900

    12. 고양이

---
+++ src/java_BOJ/BOJ_10171.java
@@ -0,0 +1,10 @@
+package java_BOJ;
+
+public class BOJ_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
