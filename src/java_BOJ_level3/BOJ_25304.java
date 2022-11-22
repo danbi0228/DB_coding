a6418aa796920fa4f6b91c6cd1b0ff2f7781ed17
danbi0228 <qoeksql97@gmail.com>
2022-11-22 21:58:11 +0900

    25304. 영수증

---
+++ src/java_BOJ_level3/BOJ_25304.java
@@ -0,0 +1,26 @@
+package java_BOJ_level3;
+
+import java.util.Scanner;
+
+public class BOJ_25304 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        int cnt = sc.nextInt();
+
+        for(int i=0; i<cnt; i++){
+            int price = sc.nextInt();
+            int count = sc.nextInt();
+
+            total = total - (price * count);
+        }
+
+        if(total == 0){
+            System.out.println("Yes");
+        }else{
+            System.out.printf("No");
+        }
+
+    }
+}
