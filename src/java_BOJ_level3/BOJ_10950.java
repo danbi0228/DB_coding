ce8c84c85720b90f748cafb955220dbfebb5904e
danbi0228 <qoeksql97@gmail.com>
2022-11-22 21:39:51 +0900

    10950. A+B-3

---
+++ src/java_BOJ_level3/BOJ_10950.java
@@ -0,0 +1,25 @@
+package java_BOJ_level3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10950 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+            sb.append('\n'); // append():문자열을 더할 때(추가할 때) 사용
+        }
+
+        System.out.println(sb);
+    }
+}
