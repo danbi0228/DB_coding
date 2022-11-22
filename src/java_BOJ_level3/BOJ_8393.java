fdc54e00749540964c26b04dd07ad1292d6cad4a
danbi0228 <qoeksql97@gmail.com>
2022-11-22 21:41:17 +0900

    8393. 합

---
+++ src/java_BOJ_level3/BOJ_8393.java
@@ -0,0 +1,18 @@
+package java_BOJ_level3;
+
+import java.util.Scanner;
+
+public class BOJ_8393 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            sum += i; // sum = sum+i;
+        }
+        System.out.println(sum);
+    }
+}
