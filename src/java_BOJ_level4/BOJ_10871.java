25738a4b1ea17cd104a6745b2e5f519dfc71d4ac
danbi0228 <qoeksql97@gmail.com>
2022-11-24 21:22:23 +0900

    10871. X보다 작은 수

---
+++ src/java_BOJ_level4/BOJ_10871.java
@@ -0,0 +1,28 @@
+package java_BOJ_level4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class BOJ_10871 {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int[] a = new int[n];
+
+        for(int i=0; i<n; i++){
+            a[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<n; i++){
+            if(a[i] < x) {
+                System.out.print(a[i] + " ");
+            }
+        }
+    }
+}
