4523b93b590057ed498bceb42ba6aaafa069559b
danbi0228 <qoeksql97@gmail.com>
2022-11-25 22:44:21 +0900

    5597. 과제 안 내신 분..?

---
+++ src/java_BOJ_level4/BOJ_5597.java
@@ -0,0 +1,24 @@
+package java_BOJ_level4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_5597 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[30];
+
+        for(int i=0; i<28; i++){
+            arr[Integer.parseInt(br.readLine())-1] = 1;
+        }
+        for(int i=0; i<30; i++){
+            if(arr[i] == 0){
+                System.out.println(i+1);
+            }
+        }
+
+    }
+}
