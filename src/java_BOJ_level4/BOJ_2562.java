5dd6ad7f1fb39f636e0c3eee4f0478ead3da8920
danbi0228 <qoeksql97@gmail.com>
2022-11-25 22:33:51 +0900

    2562. 최댓값

---
+++ src/java_BOJ_level4/BOJ_2562.java
@@ -0,0 +1,32 @@
+package java_BOJ_level4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class BOJ_2562 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[9];
+
+        for(int i=0; i<arr.length; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int max = 0, index = 0, cnt = 0;
+
+        for(int value:arr){
+            cnt++;
+            if(value > max){
+                max = value;
+                index = cnt;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+    }
+}
