06835910997908fee44a16faf55d94c8a6c5b839
danbi0228 <qoeksql97@gmail.com>
2022-11-27 19:53:49 +0900

    11654. 아스키 코드

---
+++ src/java_BOJ_level6/BOJ_11654.java
@@ -0,0 +1,14 @@
+package java_BOJ_level6;
+
+import java.util.Scanner;
+
+public class BOJ_11654 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.next().charAt(0);
+
+        System.out.println(a);
+    }
+}
