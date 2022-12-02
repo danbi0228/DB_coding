bda72f0f5832d589bd4f50a315c96c5073b16dec
danbi0228 <qoeksql97@gmail.com>
2022-12-02 11:18:06 +0900

    Lv. 0 두 수의 나눗셈

---
+++ src/programmers_level0/Exam9.java
@@ -0,0 +1,9 @@
+package programmers_level0;
+
+public class Exam9 {
+    public int solution(int num1, int num2) {
+        double answer = 0;
+        answer = (double) num1/num2 * 1000;
+        return (int)answer;
+    }
+}
