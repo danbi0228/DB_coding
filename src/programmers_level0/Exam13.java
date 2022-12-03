ecb5215a8a7b292f18efddbdf6c1a88c948a3e98
danbi0228 <qoeksql97@gmail.com>
2022-12-03 20:46:36 +0900

    Lv. 0 배열의 평균값

---
+++ src/programmers_level0/Exam13.java
@@ -0,0 +1,16 @@
+package programmers_level0;
+
+public class Exam13 {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        int sum = 0;
+
+        for(int i=0; i<numbers.length; i++){
+            sum += numbers[i];
+        }
+        answer = sum / (double)numbers.length;
+
+        return answer;
+    }
+}
+
