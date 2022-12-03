bfe0f4ccb164f3dc852cf4be82c27c37163c8ec6
danbi0228 <qoeksql97@gmail.com>
2022-12-03 21:46:29 +0900

    Lv. 0 피자 나눠 먹기 (3)

---
+++ src/programmers_level0/Exam20.java
@@ -0,0 +1,15 @@
+package programmers_level0;
+
+public class Exam20 {
+    public int solution(int slice, int n) {
+        int answer = 0;
+
+        if(n % slice == 0){
+            answer = n/slice;
+        }else{
+            answer = n/slice + 1;
+        }
+
+        return answer;
+    }
+}
