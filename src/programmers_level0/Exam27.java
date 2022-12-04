3302678da7f11975179d4c69b14753dcb31c36b0
danbi0228 <qoeksql97@gmail.com>
2022-12-04 18:02:12 +0900

    Lv. 0 편지

---
+++ src/programmers_level0/Exam27.java
@@ -0,0 +1,10 @@
+package programmers_level0;
+
+public class Exam27 {
+    public int solution(String message) {
+        int answer = 0;
+        answer = message.length() * 2;
+
+        return answer;
+    }
+}
