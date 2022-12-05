fdfb34ef6ca1e781b0c9c3c55622a7dba8ea6ce3
danbi0228 <qoeksql97@gmail.com>
2022-12-05 10:48:34 +0900

    Lv. 0 문자 반복 출력하기

---
+++ src/programmers_level0/Exam33.java
@@ -0,0 +1,14 @@
+package programmers_level0;
+
+public class Exam33 {
+    public String solution(String my_string, int n) {
+        String answer = "";
+
+        for(int i=0; i<my_string.length(); i++){
+            for(int j=0; j<n; j++){
+                answer += my_string.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
