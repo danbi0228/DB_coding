0ca205f8ced400e5a04820334ac1c1b916ef083d
danbi0228 <qoeksql97@gmail.com>
2022-12-06 22:25:54 +0900

    Lv. 1 짝수와 홀수

---
+++ src/programmers_level1/Exam01.java
@@ -0,0 +1,14 @@
+package programmers_level1;
+
+public class Exam01 {
+    public String solution(int num) {
+        String answer = "";
+
+        if(num % 2 == 0){
+            answer = "Even";
+        }else{
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
