529bbe58fdff19dfd9ba63745bf1deaa31f82599
danbi0228 <qoeksql97@gmail.com>
2022-12-08 22:11:39 +0900

    Lv. 0 세균 증식

---
+++ src/programmers_level0/Exam41.java
@@ -0,0 +1,10 @@
+package programmers_level0;
+
+public class Exam41 {
+    public int solution(int n, int t) {
+        int answer = 0;
+        answer = n * (int)Math.pow(2, t);
+
+        return answer;
+    }
+}
