ec33c23ba8ac137e8c7bef0ea99a6fcaf76d5ad1
danbi0228 <qoeksql97@gmail.com>
2022-12-19 11:56:22 +0900

    Lv. 0 최댓값 만들기 (2)

---
+++ src/programmers_level0/Exam50.java
@@ -0,0 +1,21 @@
+package programmers_level0;
+
+import java.util.Arrays;
+
+public class Exam50 {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+
+        int num1 = numbers[0] * numbers[1];
+        int num2 = numbers[numbers.length-1] * numbers[numbers.length-2];
+
+        if(num1 > num2) {
+            answer = num1;
+        }else{
+            answer = num2;
+        }
+
+        return answer;
+    }
+}
