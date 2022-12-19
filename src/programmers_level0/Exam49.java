aa47ceaa6ec650f109b5450de613708d8ea5e188
danbi0228 <qoeksql97@gmail.com>
2022-12-19 11:26:46 +0900

    Lv. 0 피자 나눠 먹기 (2)

---
+++ src/programmers_level0/Exam49.java
@@ -0,0 +1,15 @@
+package programmers_level0;
+
+public class Exam49 {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=1; i<=n; i++){
+            if((i*6)%n == 0){
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
