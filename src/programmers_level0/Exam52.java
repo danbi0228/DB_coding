cfd990ad391b897175598951927b3e028679223f
danbi0228 <qoeksql97@gmail.com>
2022-12-20 11:12:32 +0900

    Lv. 0 외계행성의 나이

---
+++ src/programmers_level0/Exam52.java
@@ -0,0 +1,14 @@
+package programmers_level0;
+
+public class Exam52 {
+    public String solution(int age) {
+        String answer = "";
+        String[] arr = Integer.toString(age).split("");
+
+        for(int i=0; i<arr.length; i++){
+            answer += (char)(Integer.parseInt(arr[i]) + 97);
+        }
+
+        return answer;
+    }
+}
