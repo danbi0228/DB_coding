caa31ad28de594fa0cab964e5c22b5cc7fc59ff3
danbi0228 <qoeksql97@gmail.com>
2022-12-21 14:06:48 +0900

    Lv. 0 가운데 글자 가져오기

---
+++ src/programmers_level1/Exam24.java
@@ -0,0 +1,17 @@
+package programmers_level1;
+
+public class Exam24 {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split("");
+
+        for(int i=0; i<arr.length; i++){
+            if(arr.length % 2 != 0){
+                answer = arr[arr.length / 2];
+            }else{
+                answer = arr[arr.length / 2 - 1] + arr[arr.length / 2];
+            }
+        }
+        return answer;
+    }
+}
