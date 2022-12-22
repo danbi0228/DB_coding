81b38de33926109ab82d2434a66063edab60b783
danbi0228 <qoeksql97@gmail.com>
2022-12-22 13:51:36 +0900

    Lv. 1 수박수박수박수박수박수?

---
+++ src/programmers_level1/Exam28.java
@@ -0,0 +1,16 @@
+package programmers_level1;
+
+public class Exam28 {
+    public String solution(int n) {
+        String answer = "";
+
+        for(int i=1; i<=n; i++){
+            if(i%2 != 0){
+                answer += "수";
+            }else{
+                answer += "박";
+            }
+        }
+        return answer;
+    }
+}
