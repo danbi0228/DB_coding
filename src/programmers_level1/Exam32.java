c21dad9de86a53240657f1cd37c91bed873e2a0a
danbi0228 <qoeksql97@gmail.com>
2022-12-23 11:31:02 +0900

    Lv. 1 예산

---
+++ src/programmers_level1/Exam32.java
@@ -0,0 +1,19 @@
+package programmers_level1;
+
+import java.util.Arrays;
+
+public class Exam32 {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+
+        for(int i=0; i<d.length; i++){
+            budget -= d[i];
+            if(budget < 0){
+                break;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
