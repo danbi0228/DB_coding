3f789e07384d0935fcb58e271a0260ca6ca14679
danbi0228 <qoeksql97@gmail.com>
2022-12-28 15:35:39 +0900

    Lv. 1 삼총사

---
+++ src/programmers_level1/Exam37.java
@@ -0,0 +1,18 @@
+package programmers_level1;
+
+public class Exam37 {
+    public int solution(int[] number) {
+        int answer = 0;
+
+        for(int i=0; i<number.length; i++){
+            for(int j=i+1; j<number.length; j++){
+                for(int k=j+1; k<number.length; k++){
+                    if(number[i]+number[j]+number[k] == 0){
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
