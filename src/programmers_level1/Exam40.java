b70e04cc253938cd1252f34ba5581831b41aeed8
danbi0228 <qoeksql97@gmail.com>
2022-12-29 13:52:45 +0900

    Lv. 1 소수 찾기

---
+++ src/programmers_level1/Exam40.java
@@ -0,0 +1,28 @@
+package programmers_level1;
+
+public class Exam40 {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n+1];
+
+        arr[0] = arr[1] = 0;
+
+        for(int i=2; i<=n; i++){
+            arr[i] = i;
+        }
+
+        for(int i=2; i<n; i++){
+            if(arr[i] == 0) continue;
+            for(int j=2*i; j<=n; j+=i){
+                arr[j] = 0;
+            }
+        }
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] != 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
