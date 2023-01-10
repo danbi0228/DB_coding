ed3cd15b46f43943bfe679ccbed18bb41cd2badd
danbi0228 <qoeksql97@gmail.com>
2023-01-10 20:41:23 +0900

    Lv. 2 구명보트

---
+++ src/programmers_level2/Exam09.java
@@ -0,0 +1,23 @@
+package programmers_level2;
+
+import java.util.Arrays;
+
+public class Exam09 {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int idx = 0;
+
+        Arrays.sort(people);
+
+        for(int i=people.length-1; i >= idx; i--){
+            if(people[i] + people[idx] <= limit){
+                idx++;
+                answer++;
+            }else{
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
