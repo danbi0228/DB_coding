9153b3fe8b9aacdea75cf6d65769669f4d05ca80
danbi0228 <qoeksql97@gmail.com>
2023-01-12 15:23:44 +0900

    2587. 대표값2

---
+++ src/java_BOJ_level10/Exam2587.java
@@ -0,0 +1,23 @@
+package java_BOJ_level10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Exam2587 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[5];
+        int sum = 0;
+
+        for(int i=0; i<arr.length; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+
+        System.out.println(sum / arr.length);
+        System.out.println(arr[2]);
+    }
+}
