d5688eb190daea9125ae1f46418619cece047303
danbi0228 <qoeksql97@gmail.com>
2023-01-13 09:55:31 +0900

    2751. 수 정렬하기 2

---
+++ src/java_BOJ_level10/BOJ_2751.java
@@ -0,0 +1,25 @@
+package java_BOJ_level10;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class BOJ_2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for(int i=0; i<arr.length; i++){
+            bw.write(arr[i] + "\n");
+        }
+
+        bw.flush();
+    }
+}
