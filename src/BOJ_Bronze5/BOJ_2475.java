ee3600712fcf6ac664b3071f8c1758fd9ce65cfd
danbi0228 <qoeksql97@gmail.com>
2023-01-15 13:25:23 +0900

    2475. 검증수

---
+++ src/BOJ_Bronze5/BOJ_2475.java
@@ -0,0 +1,21 @@
+package BOJ_Bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = 0;
+
+        for(int i=0; i<5; i++){
+            sum += Math.pow(Integer.parseInt(st.nextToken()), 2);
+        }
+
+        System.out.println(sum % 10);
+    }
+}
