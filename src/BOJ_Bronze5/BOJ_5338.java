55852c1e533dd3cdc3f56dd7d47b26ccb66f7a8f
danbi0228 <qoeksql97@gmail.com>
2023-01-17 16:08:10 +0900

    5338. 마이크로소프트 로고

---
+++ src/BOJ_Bronze5/BOJ_5338.java
@@ -0,0 +1,11 @@
+package BOJ_Bronze5;
+
+public class BOJ_5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
