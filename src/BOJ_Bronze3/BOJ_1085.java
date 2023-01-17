7131086f60bcb890da6f887a7c35f8d833941335
danbi0228 <qoeksql97@gmail.com>
2023-01-17 16:10:09 +0900

    1085. 직사각형에서 탈출

---
+++ src/BOJ_Bronze3/BOJ_1085.java
@@ -0,0 +1,22 @@
+package BOJ_Bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1085 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int vertical = Math.min(w-x, x);
+        int horizon = Math.min(h-y, y);
+
+        System.out.println(Math.min(vertical, horizon));
+    }
+}
