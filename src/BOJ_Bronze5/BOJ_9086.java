eac59f627d6a124034961ba49a388a6f4a557ae4
danbi0228 <qoeksql97@gmail.com>
2023-01-18 14:49:14 +0900

    9086. 문자열

---
+++ src/BOJ_Bronze5/BOJ_9086.java
@@ -0,0 +1,17 @@
+package BOJ_Bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_9086 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++){
+            String s = br.readLine();
+            System.out.println(s.charAt(0) + "" + s.charAt(s.length()-1));
+        }
+    }
+}
