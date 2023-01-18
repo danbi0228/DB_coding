f53223eb909f398d66358851974a5b00d9b94c15
danbi0228 <qoeksql97@gmail.com>
2023-01-18 14:51:14 +0900

    3046. R2

---
+++ src/BOJ_Bronze4/BOJ_3046.java
@@ -0,0 +1,18 @@
+package BOJ_Bronze4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_3046 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int R1 = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        System.out.println(S*2 - R1);
+    }
+}
