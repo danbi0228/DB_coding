f9120b2dce1719b41eda5ea6ee0274f6cef03f54
danbi0228 <qoeksql97@gmail.com>
2023-01-19 10:59:06 +0900

    10699. 오늘 날짜

---
+++ src/BOJ_Bronze5/BOJ_10699.java
@@ -0,0 +1,17 @@
+package BOJ_Bronze5;
+
+import java.time.LocalDate;
+
+public class BOJ_10699 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+
+//        Calendar today = Calendar.getInstance();
+//        int year = today.get(Calendar.YEAR);
+//        int month = today.get(Calendar.MONTH);
+//        int day = today.get(Calendar.DATE);
+//
+//        System.out.println(year + "-" + month + 1 + "-" + day);
+    }
+}
