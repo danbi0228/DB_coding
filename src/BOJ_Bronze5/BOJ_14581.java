c2df79e17957c8140f878044bab0185c5dca8de2
danbi0228 <qoeksql97@gmail.com>
2023-01-26 14:59:04 +0900

    14581. 팬들에게 둘러싸인 홍준

---
+++ src/BOJ_Bronze5/BOJ_14581.java
@@ -0,0 +1,16 @@
+package BOJ_Bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_14581 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String id = br.readLine();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"+id+"::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
