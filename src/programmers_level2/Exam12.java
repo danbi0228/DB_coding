ff9a31ecaaae8110b20170589d66b8d70f3ffe7e
danbi0228 <qoeksql97@gmail.com>
2023-02-10 16:29:45 +0900

    Lv. 2 카펫

---
+++ src/programmers_level2/Exam12.java
@@ -0,0 +1,20 @@
+package programmers_level2;
+
+public class Exam12 {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int sum = brown + yellow;
+
+        for(int i=3; i<sum; i++) {
+            int width = sum / i;
+            if(width >= i) {
+                if((i - 2) * (width - 2) == yellow) {
+                    answer[0] = width;
+                    answer[1] = i;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
