529a80c788b187a2aae8e17363c6c488deaee0ef
danbi0228 <qoeksql97@gmail.com>
2023-03-17 20:28:23 +0900

    1018. 체스판 다시 칠하기

---
+++ src/BOJ_Silver4/BOJ_1018.java
@@ -0,0 +1,62 @@
+package BOJ_Silver4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1018 {
+    public static boolean[][] arr;
+    public static int min = 64;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new boolean[N][M];
+
+        for(int i=0; i<N; i++) {
+            String s = br.readLine();
+            for(int j=0; j<M; j++) {
+                if (s.charAt(j) == 'W') {
+                    arr[i][j] = true;
+                }else {
+                    arr[i][j] = false;
+                }
+            }
+        }
+
+        int row = N - 7;
+        int col = M - 7;
+
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                find(i, j);
+            }
+        }
+        System.out.println(min);
+    }
+
+    public static void find(int x, int y) {
+        int end_x = x + 8;
+        int end_y = y + 8;
+        int cnt = 0;
+
+        boolean TF = arr[x][y];
+
+        for(int i=x; i<end_x; i++) {
+            for(int j=y; j<end_y; j++) {
+                if (arr[i][j] != TF) {
+                    cnt++;
+                }
+                TF = !TF;
+            }
+            TF = !TF;
+        }
+        cnt = Math.min(cnt, 64 - cnt);
+        min = Math.min(min, cnt);
+    }
+}
