30be2901029dde3fe24e60757acf840810bcba17
danbi0228 <qoeksql97@gmail.com>
2023-03-21 21:16:34 +0900

    1934. 최소공배수

---
+++ src/BOJ_Bronze1/BOJ_1934.java
@@ -0,0 +1,28 @@
+package BOJ_Bronze1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1934 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for(int i=0; i<T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            System.out.println(A *  B / gcd(A, B));
+        }
+    }
+
+    public static int gcd(int A, int B) {
+        if (B == 0) {
+            return A;
+        }
+        return gcd(B, A % B);
+    }
+}
