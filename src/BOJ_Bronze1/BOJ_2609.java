7f18763d880376c43679adb5eb69aa0756754e71
danbi0228 <qoeksql97@gmail.com>
2023-03-23 19:04:56 +0900

    2609. 최대공약수와 최소공배수

---
+++ src/BOJ_Bronze1/BOJ_2609.java
@@ -0,0 +1,26 @@
+package BOJ_Bronze1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2609 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(gcd(A, B));
+        System.out.println(A * B / gcd(A, B));
+    }
+
+    public static int gcd(int A, int B) {
+        if (B == 0) {
+            return A;
+        }
+        return gcd(B, A % B);
+    }
+}
