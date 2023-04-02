76e074c0b40c76430f0fdd46fc2ba6a70d11e8ab
danbi0228 <qoeksql97@gmail.com>
2023-04-02 21:30:52 +0900

    1927. 최소 힙

---
+++ src/BOJ_Silver2/BOJ_1927.java
@@ -0,0 +1,27 @@
+package BOJ_Silver2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BOJ_1927 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for(int i=0; i<N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                if (!queue.isEmpty()) {
+                    System.out.println(queue.poll());
+                } else {
+                    System.out.println(0);
+                }
+                continue;
+            }
+            queue.add(num);
+        }
+    }
+}
